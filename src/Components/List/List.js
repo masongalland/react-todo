34a7b0bf78cc90815035db73828c4d3e9d4ac878
Mason Galland <masongalland@me.com>
2017-07-11 16:38:23 -0600

    clicking the complete button in the list component now changes the completed property on state

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
-
-import ListItem from "../ListItem/ListItem";
+import {markCompleted} from '../../reducer';
 
 import "./List.css";
+import "./ListItem.css";
 
 class List extends Component {
     render() {
 
-        const todos = this.props.todos.map((todo) => (
-            <ListItem task={todo.task} />
+        const todos = this.props.todos.map((todo, index) => (
+            <div key={index}className="list_item_container">
+                <p>{todo.task}</p>
+                <div className="quick_options">
+                    <button className="complete_button" onClick={() => this.props.markCompleted(index)}>Complete</button>
+                    <p>X</p>
+                </div>
+            </div>
         ))
 
         return (
@@ -23,4 +29,4 @@ function mapStateToProps(state) {
     return state
 }
 
-export default connect(mapStateToProps)(List);
+export default connect(mapStateToProps, {markCompleted})(List);
--- src/reducer.js
+++ src/reducer.js
@@ -3,33 +3,48 @@ const initialState = {
         {
             task: "Take the dog for a walk",
             description: "Take Georgie out again. He hasn't been walked in weeks.",
-            completed: "false"
+            completed: false
         },
         {
             task: "Take out the trash",
             description: "I forgot to take it out last week. It is piling up.",
-            completed: "false"
+            completed: false
         }
     ],
     selected: null,
     newTaskInput: ''
 }
 
-const TYPE_NEW_TASK = "TYPE_NEW_TASK"
+const TYPE_NEW_TASK = "TYPE_NEW_TASK";
 const ADD_TASK = "ADD_TASK";
+const COMPLETE_TASK = "COMPLETE_TASK";
 
 
 function reducer(state=initialState, action){
     switch(action.type){
         case ADD_TASK:
+            console.log('new')
+        
             return Object.assign({}, state, { todos: [...state.todos, {
                 task: action.payload,
                 description: '',
-                completed: 'false'
+                completed: false
             }], newTaskInput: ''} )
         case TYPE_NEW_TASK:
+            console.log('typing')
+        
             return Object.assign({}, state, {newTaskInput: action.payload})
+        case COMPLETE_TASK:
+            let filtered = state.todos.map((todo, index) => {
+                if(index === action.payload){
+                    todo.completed = true;
+                } 
+                return todo;
+            });
+            return Object.assign( {}, state, {todos: filtered} )
         default:
+            console.log('default')
+        
             return state;
     }
     
@@ -46,6 +61,14 @@ export function addTask(task){
     return {
         type: ADD_TASK,
         payload: task
+    }
+}
+
+export function markCompleted(index){
+    // console.log(index)
+    return {
+        type: COMPLETE_TASK,
+        payload: index
     }
 }
 
