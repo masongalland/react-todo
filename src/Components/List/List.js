5ba9d1cc40eaf783492cf2d4a516c15a20b0efda
Mason Galland <masongalland@me.com>
2017-07-11 16:57:56 -0600

    added conditional styling based on completed value on state

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -7,12 +7,12 @@ import "./ListItem.css";
 
 class List extends Component {
     render() {
-
+        
         const todos = this.props.todos.map((todo, index) => (
-            <div key={index}className="list_item_container">
-                <p>{todo.task}</p>
+            <div key={index} className={"list_item_container " + (todo.completed ? "completed_container" : "list_item_container")}>
+                <p className={todo.completed ? "completed" : "" }>{todo.task}</p>
                 <div className="quick_options">
-                    <button className="complete_button" onClick={() => this.props.markCompleted(index)}>Complete</button>
+                    <button className={"complete_button " + (todo.completed ? "completed" : "")  }onClick={() => this.props.markCompleted(index)}>Complete</button>
                     <p>X</p>
                 </div>
             </div>
